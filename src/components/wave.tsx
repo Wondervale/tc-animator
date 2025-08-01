968437bbb76b1339d67f0106f358bdd0b0cbb4c6
Foxxite <foxxite@foxxite.com>
2025-08-01 16:58:35 +0200

    Refactor code structure for improved readability and maintainability

--- src/components/wave.tsx
+++ src/components/wave.tsx
@@ -6,11 +6,26 @@
  */
 
 import { Input } from "@/components/ui/input";
+import type WaveSurfer from "wavesurfer.js";
 import WavesurferPlayer from "@wavesurfer/react";
 import { useState } from "react";
 
 function Wave() {
 	const [audioFile, setAudioFile] = useState<File | null>(null);
+
+	const [wavesurfer, setWavesurfer] = useState<WaveSurfer | null>(null);
+	const [isPlaying, setIsPlaying] = useState(false);
+
+	const onReady = (ws: WaveSurfer) => {
+		setWavesurfer(ws);
+		setIsPlaying(false);
+	};
+
+	const onPlayPause = () => {
+		if (wavesurfer) {
+			wavesurfer.playPause();
+		}
+	};
 
 	return (
 		<div>
@@ -25,7 +40,14 @@ function Wave() {
 				}}
 			/>
 
-			<WavesurferPlayer url={audioFile ? URL.createObjectURL(audioFile) : ""} waveColor={"#3a81f6"} />
+			<WavesurferPlayer
+				url={audioFile ? URL.createObjectURL(audioFile) : ""}
+				waveColor={"#3a81f6"}
+				onReady={onReady}
+				onPlay={() => setIsPlaying(true)}
+				onPause={() => setIsPlaying(false)}
+			/>
+			<button onClick={onPlayPause}>{isPlaying ? "Pause" : "Play"}</button>
 		</div>
 	);
 }
