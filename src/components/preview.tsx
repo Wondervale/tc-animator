d4adc1d64d7f3dd37ba4f9a273e34cf39766ec65
Foxxite <foxxite@foxxite.com>
2025-08-02 15:33:53 +0200

    Render dummy in previw, use missing texture for blocks, enable some postprocessing

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -5,30 +5,44 @@
  * @format
  */
 
+import * as THREE from "three";
+
 import {
-	AccumulativeShadows,
 	Box,
 	Environment,
 	GizmoHelper,
 	GizmoViewport,
 	Grid,
 	OrbitControls,
-	RandomizedLight,
+	useGLTF,
+	useTexture,
 } from "@react-three/drei";
+import { EffectComposer, FXAA, SSAO } from "@react-three/postprocessing";
 
 import { Canvas } from "@react-three/fiber";
-import { memo } from "react";
 
 function Preview() {
 	return (
 		<div className="w-full h-full relative">
 			<Canvas camera={{ position: [2, 2, 2], fov: 60 }} shadows>
 				<ambientLight intensity={0.5} />
-				<directionalLight position={[5, 5, 5]} intensity={0.5} />
+				<directionalLight
+					position={[5, 5, 5]}
+					intensity={1}
+					castShadow
+					shadow-mapSize-width={1024}
+					shadow-mapSize-height={1024}
+					shadow-camera-near={1}
+					shadow-camera-far={10}
+					shadow-camera-left={-5}
+					shadow-camera-right={5}
+					shadow-camera-top={5}
+					shadow-camera-bottom={-5}
+				/>
 
-				<Box args={[1, 1, 1]} position={[0, 0, 0]} />
+				<Cube args={[1, 1, 1]} position={[0, 0.5, 0]} />
 
-				<Shadows />
+				<Dummy scale={1} position={[0, 0.5, 0]} />
 
 				<Grid
 					position={[0.5, 0, 0.5]}
@@ -41,10 +55,24 @@ function Preview() {
 					fadeDistance={100}
 					followCamera={false}
 					infiniteGrid
+					castShadow={false}
+					receiveShadow={false}
 				/>
 
-				<OrbitControls makeDefault enableDamping={false} />
 				<Environment preset="city" />
+
+				<EffectComposer>
+					<SSAO
+						samples={31}
+						radius={20}
+						intensity={20}
+						luminanceInfluence={0.9}
+						color={new THREE.Color("black")}
+					/>
+					<FXAA />
+				</EffectComposer>
+
+				<OrbitControls makeDefault enableDamping={false} />
 				<GizmoHelper alignment="bottom-right" margin={[80, 80]}>
 					<GizmoViewport axisColors={["#9d4b4b", "#2f7f4f", "#3b5b9d"]} labelColor="white" />
 				</GizmoHelper>
@@ -53,10 +81,34 @@ function Preview() {
 	);
 }
 
-const Shadows = memo(() => (
-	<AccumulativeShadows temporal frames={100} color="#9d4b4b" colorBlend={0.5} alphaTest={0.9} scale={20}>
-		<RandomizedLight amount={8} radius={4} position={[5, 5, -10]} />
-	</AccumulativeShadows>
-));
+function Cube({ args, position }: { args: [number, number, number]; position: [number, number, number] }) {
+	// This is a simple cube component that can be used in the scene.
+
+	const originalTexture = useTexture("/textures/missing.png");
+	const texture = originalTexture.clone();
+	texture.magFilter = THREE.NearestFilter;
+	texture.minFilter = THREE.NearestFilter;
+
+	return (
+		<Box args={args} position={position} castShadow receiveShadow>
+			<meshBasicMaterial map={texture} color="#92a3bb" />
+		</Box>
+	);
+}
+
+function Dummy({ scale = 0.01, position = [0, 0, 0] }: { scale?: number; position?: [number, number, number] }) {
+	const dummyModel = useGLTF("/models/dummy.glb");
+
+	return (
+		<primitive
+			object={dummyModel.scene}
+			scale={scale}
+			position={position}
+			castShadow
+			receiveShadow
+			dispose={null}
+		/>
+	);
+}
 
 export default Preview;
--- vite.config.ts
+++ vite.config.ts
@@ -21,4 +21,9 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
+	build: {
+		rollupOptions: {
+			treeshake: true,
+		},
+	},
 });
