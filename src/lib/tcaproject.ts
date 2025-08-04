b0e342ebfc3a49dc22c1d92e376f7246a1a1d2d1
Foxxite <Foxxite@users.noreply.github.com>
2025-08-04 20:51:55 +0000

    deploy: 8c6548456ddb3618df9dfaa1e8a1b4b1948bc952

--- src/lib/tcaproject.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2025 Foxxite | Articca
- *   All rights reserved.
- *
- * @format
- */
-
-// The file header of .tcap files. Easter-egg as: 🦊🚂🎬
-export const fileHeader = new Uint8Array([0xa6, 0x8a, 0x9a, 0x82, 0x8e, 0xac]);
