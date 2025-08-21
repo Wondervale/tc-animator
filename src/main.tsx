88611a190450584de270858f78e1a5e96da038f5
Foxxite <foxxite@foxxite.com>
2025-08-21 20:04:43 +0200

    feat: integrate PostHog for analytics tracking in main application; update main.tsx to include PostHogProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 
 import App from "./App.tsx";
 import CheckBrowserSupport from "./components/CheckBrowserSupport.tsx";
+import { PostHogProvider } from "posthog-js/react";
 import ProjectDialog from "@/components/ProjectDialog.tsx";
 import { StrictMode } from "react";
 import { Toaster } from "sonner";
@@ -11,11 +12,20 @@ import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<CheckBrowserSupport />
-		<ProjectDialog />
+		<PostHogProvider
+			apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+			options={{
+				api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+				defaults: "2025-05-24",
+				capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+				debug: import.meta.env.MODE === "development",
+			}}>
+			<CheckBrowserSupport />
+			<ProjectDialog />
 
-		<App />
+			<App />
 
-		<Toaster position="top-center" />
+			<Toaster position="top-center" />
+		</PostHogProvider>
 	</StrictMode>
 );
