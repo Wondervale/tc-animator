ab42240776513b6b209f24fd1a4fc0bea59fd073
Foxxite <foxxite@foxxite.com>
2025-08-21 21:01:20 +0200

    feat: add Tooltip component and integrate it into PreferencesPanel; update styles and functionality for improved user experience

--- src/components/AppMenu.tsx
+++ src/components/AppMenu.tsx
@@ -5,16 +5,8 @@
  * @format
  */
 
-import {
-	Dialog,
-	DialogClose,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-} from "@/components/ui/dialog";
-import { Maximize, Minimize, X } from "lucide-react";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Info, Maximize, Minimize, X } from "lucide-react";
 import {
 	Menubar,
 	MenubarContent,
@@ -33,12 +25,14 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from "@/components/ui/select";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import ColorPicker from "@/components/ColorPicker";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
 import { usePreferences } from "@/stores/PreferencesStore";
 import { useProjectStore } from "@/stores/ProjectStore";
 
@@ -173,72 +167,137 @@ function PreferencesPanel({ open, setOpen }: { open: boolean; setOpen: (open: bo
 			<DialogContent>
 				<DialogHeader>
 					<DialogTitle>Preferences</DialogTitle>
-					<DialogDescription>
-						Here you can adjust your preferences for the application.
-						<div className="mt-4">
-							<Label htmlFor="theme-select" className="block mb-2">
-								Theme:
-							</Label>
-							<Select
-								value={preferences.theme}
-								onValueChange={(value) => {
-									if (value !== "light" && value !== "dark") {
-										console.warn("Invalid theme selected:", value);
-										return;
-									}
+					<DialogDescription asChild>
+						<div>
+							Here you can adjust your preferences for the application.
+							<div className="grid grid-cols-3 gap-4 mt-4">
+								<div className="flex flex-col gap-2  justify-evenly">
+									<Label htmlFor="theme-select" className="block mb-2">
+										Theme:
+									</Label>
+									<Select
+										value={preferences.theme}
+										onValueChange={(value) => {
+											if (value !== "light" && value !== "dark") {
+												console.warn("Invalid theme selected:", value);
+												return;
+											}
+											preferences.setTheme(value);
+										}}>
+										<SelectTrigger className="w-full h-10" id="theme-select">
+											<SelectValue placeholder="Select theme" />
+										</SelectTrigger>
+										<SelectContent>
+											<SelectGroup>
+												<SelectLabel>Available Themes</SelectLabel>
+												<SelectItem value="light">Light</SelectItem>
+												<SelectItem value="dark">Dark</SelectItem>
+											</SelectGroup>
+										</SelectContent>
+									</Select>
+								</div>
+								<div className="flex flex-col gap-2  justify-evenly">
+									<Label htmlFor="save-interval" className="block mb-2">
+										Auto-save Interval (seconds):
+									</Label>
+									<Input
+										id="save-interval"
+										type="number"
+										min={1}
+										value={preferences.saveInterval}
+										onChange={(e) => {
+											const value = parseInt(e.target.value, 10);
+											if (isNaN(value) || value < 1) {
+												console.warn("Invalid save interval:", e.target.value);
+												return;
+											}
+											preferences.setSaveInterval(value);
+										}}
+										className="w-full h-10"
+									/>
+								</div>
+								<div className="flex flex-col gap-2  justify-evenly">
+									<Label htmlFor="grid-color" className="block mb-2">
+										Grid Color:
+									</Label>
+									<ColorPicker
+										defaultColor={preferences.gridColor}
+										onChangeComplete={(color) => {
+											preferences.setGridColor(color.hex);
+										}}
+									/>
+								</div>
+								<div className="flex flex-col gap-2  justify-evenly">
+									<Label htmlFor="debug-text" className="block mb-2">
+										<Tooltip>
+											<TooltipTrigger asChild>
+												<Info className="inline mr-[0.5] h-3" />
+											</TooltipTrigger>
+											<TooltipContent>
+												<p>
+													Debug text displays additional information about the 3D scene, such
+													as object coordinates and rendering details. It can be useful for
+													troubleshooting but may clutter the view.
+												</p>
+											</TooltipContent>
+										</Tooltip>
+										Debug Text:
+									</Label>
+									<Switch
+										id="debug-text"
+										checked={preferences.debugText}
+										onCheckedChange={(checked) => preferences.setDebugText(checked)}
+									/>
+								</div>
+								<div className="flex flex-col gap-2  justify-evenly">
+									<Label htmlFor="ssao-enabled" className="block mb-2">
+										<Tooltip>
+											<TooltipTrigger asChild>
+												<Info className="inline mr-[0.5] h-3" />
+											</TooltipTrigger>
+											<TooltipContent>
+												<p>
+													Screen Space Ambient Occlusion (SSAO) is a shading method that adds
+													soft shadows to enhance depth perception in 3D scenes. It can
+													improve visual realism but may impact performance.
+												</p>
+											</TooltipContent>
+										</Tooltip>
+										SSAO Enabled:
+									</Label>
+									<Switch
+										id="ssao-enabled"
+										checked={preferences.SSAOEnabled}
+										onCheckedChange={(checked) => preferences.setSSAOEnabled(checked)}
+									/>
+								</div>
+								<div className="flex flex-col gap-2 justify-evenly">
+									<Label htmlFor="fxaa-enabled" className="block mb-2">
+										<Tooltip>
+											<TooltipTrigger asChild>
+												<Info className="inline mr-[0.5] h-3" />
+											</TooltipTrigger>
+											<TooltipContent>
+												<p>
+													Fast Approximate Anti-Aliasing (FXAA) is a post-processing technique
+													that smooths out jagged edges in 3D graphics. It can improve visual
+													quality but may impact performance.
+												</p>
+											</TooltipContent>
+										</Tooltip>
+										FXAA Enabled:
+									</Label>
 
-									preferences.setTheme(value);
-								}}>
-								<SelectTrigger className="w-full" id="theme-select">
-									<SelectValue placeholder="Select theme" />
-								</SelectTrigger>
-								<SelectContent>
-									<SelectGroup>
-										<SelectLabel>Available Themes</SelectLabel>
-										<SelectItem value="light">Light</SelectItem>
-										<SelectItem value="dark">Dark</SelectItem>
-									</SelectGroup>
-								</SelectContent>
-							</Select>
-						</div>
-						<div className="mt-4">
-							<Label htmlFor="save-interval" className="block mb-2">
-								Auto-save Interval (seconds):
-							</Label>
-							<Input
-								id="save-interval"
-								type="number"
-								min={1}
-								value={preferences.saveInterval}
-								onChange={(e) => {
-									const value = parseInt(e.target.value, 10);
-									if (isNaN(value) || value < 1) {
-										console.warn("Invalid save interval:", e.target.value);
-										return;
-									}
-									preferences.setSaveInterval(value);
-								}}
-								className="w-full"
-							/>
-						</div>
-						<div className="mt-4">
-							<Label htmlFor="grid-color" className="block mb-2">
-								Grid Color:
-							</Label>
-							<ColorPicker
-								defaultColor={preferences.gridColor}
-								onChangeComplete={(color) => {
-									preferences.setGridColor(color.hex);
-								}}
-							/>
+									<Switch
+										id="fxaa-enabled"
+										checked={preferences.FXAAEnabled}
+										onCheckedChange={(checked) => preferences.setFXAAEnabled(checked)}
+									/>
+								</div>
+							</div>
 						</div>
 					</DialogDescription>
 				</DialogHeader>
-				<DialogFooter>
-					<DialogClose asChild>
-						<Button type="button">Save</Button>
-					</DialogClose>
-				</DialogFooter>
 			</DialogContent>
 		</Dialog>
 	);
